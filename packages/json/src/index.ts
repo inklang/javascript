87a363ec68673ca5f685b35a6b090607bb671aae
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-22 17:54:13 +0100

    feat(json): add optional functions

--- packages/json/src/index.ts
+++ packages/json/src/index.ts
@@ -7,12 +7,20 @@ export const asArray = (v: Value): Array<Value> => v;
 export const asString = (v: Value): string => v;
 export const asBoolean = (v: Value): boolean => v;
 
+export const asOptionalArray = (v: Value): Array<Value> | undefined | null => v;
+export const asOptionalString = (v: Value): string | undefined | null => v;
+export const asOptionalBoolean = (v: Value): boolean | undefined | null => v;
+
 const asNumber = (v: Value): number => v;
+const asOptionalNumber = (v: Value): number | undefined | null => v;
 
 export {
   asNumber as asU64,
   asNumber as asF64,
   asNumber as asI64,
+  asOptionalNumber as asOptionalU64,
+  asOptionalNumber as asOptionalF64,
+  asOptionalNumber as asOptionalI64,
 };
 
 export const isDefined = (value: Value): boolean => {
